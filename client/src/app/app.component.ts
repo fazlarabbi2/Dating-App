933bc3c9145c143103c187891979c9c8497e5cdc
fazlarabbi2 <rabbifazla4@gmail.com>
2024-11-26 03:44:31 +0600

    HTTPs implemented

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -16,7 +16,7 @@ export class AppComponent implements OnInit {
 
 
   ngOnInit(): void {
-    this.http.get("https://localhost:7225/api/users").subscribe({
+    this.http.get("http://localhost:5183/api/users").subscribe({
       next: response => this.users = response,
       error: error => console.log(error),
       complete: () => console.log("Reqeust has been complete")
