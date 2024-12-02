2c28e0ba294f25820d8e78ea0350d9f417e2b07d
fazlarabbi2 <rabbifazla4@gmail.com>
2024-12-03 05:11:52 +0600

    Implement user authentication with account service and update navigation component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from "./nav/nav.component";
+import { AccountService } from './services/account.service';
 
 @Component({
   selector: 'app-root',
@@ -12,11 +13,26 @@ import { NavComponent } from "./nav/nav.component";
 })
 export class AppComponent implements OnInit {
   title = 'Dating Application';
+  private accountService = inject(AccountService);
   http = inject(HttpClient);
   users: any;
 
-
   ngOnInit(): void {
+    this.getUser();
+    this.setCurrentUser();
+  }
+
+
+  setCurrentUser(){
+    const userString = localStorage.getItem('user');
+
+    if(!userString) return;
+
+    const user = JSON.parse(userString);
+    this.accountService.currentUser.set(user);
+  }
+
+  getUser(){
     this.http.get("http://localhost:5183/api/users").subscribe({
       next: response => this.users = response,
       error: error => console.log(error),
